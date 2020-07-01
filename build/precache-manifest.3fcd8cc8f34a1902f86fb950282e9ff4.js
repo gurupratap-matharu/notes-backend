f81ad1ac7766a5126fbe155a830b18334de86e60
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-01 19:35:04 -0300

    this is the React frontend build

---
+++ build/precache-manifest.3fcd8cc8f34a1902f86fb950282e9ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd567b4d562ef2c83692e7d40445d62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0023c5ca45904531b9ea",
+    "url": "/static/css/main.f53a681e.chunk.css"
+  },
+  {
+    "revision": "2bca4a2f2c8e576ef16a",
+    "url": "/static/js/2.e38ffbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e38ffbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0023c5ca45904531b9ea",
+    "url": "/static/js/main.2be1a6f8.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
---
+++ build/precache-manifest.5069e401a0d9027feb892d1df3e05b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1276243576278bca25d3509565f2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca8a8cb8a411e58ea8e",
+    "url": "/static/css/main.f53a681e.chunk.css"
+  },
+  {
+    "revision": "2bca4a2f2c8e576ef16a",
+    "url": "/static/js/2.e38ffbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e38ffbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca8a8cb8a411e58ea8e",
+    "url": "/static/js/main.74c2a56e.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
