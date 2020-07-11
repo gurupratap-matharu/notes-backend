1c32f29cf291633efb4902bc8755aee1e214dec5
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-10 21:10:14 -0300

    formatting file with prettier now ;D

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const Note = require('./models/note')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const Note = require("./models/note");
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-app.get('/api/notes', (req, res) => {
-    Note
-        .find({})
-        .then(notes => {
-            res.json(notes)
-        })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            response.json(note)
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(404).end()
-        })
-})
-
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({
-            error: "content missing"
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        date: new Date(),
-        important: body.important || false,
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
     })
+    .catch((error) => next(error));
+});
 
-    note
-        .save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-})
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
-})
+  if (body.content === undefined) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
 
-let PORT = process.env.PORT
+  const note = new Note({
+    content: body.content,
+    date: new Date(),
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
+let PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
