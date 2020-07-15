3911240d71303979c9b5bcc737e477e6a39273a4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-15 12:29:13 -0300

    complete refactor to import modules

--- index.js
+++ index.js
@@ -1,104 +1,10 @@
 /* eslint-disable indent */
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const Note = require('./models/note')
+const app = require('./app')
 const http = require('http')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 const server = http.createServer(app)
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
-})
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
-
-  const note = new Note({
-    content: body.content,
-    date: new Date(),
-    important: body.important || false,
-  })
-
-  note
-    .save()
-    .then((savedNote) => savedNote.toJSON())
-    .then((savedAndFormattedNote) => {
-      response.json(savedAndFormattedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote)
-    })
-    .catch((error) => next(error))
-})
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-
-app.use(errorHandler)
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
