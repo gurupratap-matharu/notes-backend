2bb0fb4925c63eab2dd22d83a9c8ad10e8f60dfd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-15 12:28:10 -0300

    bug in unknown endpoint

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 
-app.use(middleware.unknownEndpoint)
+app.use(middleware.unknownEndpont)
 app.use(middleware.errorHandler)
 
 module.exports = app
