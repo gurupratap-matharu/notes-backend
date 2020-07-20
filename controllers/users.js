1638247a968ae07e910710006a7f86d056d29e17
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-20 11:36:01 -0300

    added post endpoint for registering a new user

---
+++ controllers/users.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/:id', async (request, response) => {
+    const user = await User.findById(request.params.id)
+
+    if (user) {
+        response.json(user)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+    response.json(savedUser)
+})
+
+module.exports = usersRouter
