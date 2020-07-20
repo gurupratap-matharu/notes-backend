b63e6c3fbf723281998c13134dbc37bcee51cf9f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-20 16:08:17 -0300

    corrected spelling for unknown endpoint

--- app.js
+++ app.js
@@ -20,16 +20,14 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     })
 
 app.use(cors())
-
-app.use(express.static('build'))
 app.use(express.json())
-
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
-app.use(middleware.unknownEndpont)
+app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(express.static('build'))
 
 module.exports = app
